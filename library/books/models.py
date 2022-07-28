3a0d8087d02856ddfc05113cfdd19384eef8dc76
omen <andrea.verdura3@gmail.com>
2022-07-28 22:16:56 +0200

    Added api

--- library/books/models.py
+++ library/books/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=250)
-    subtitile = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
     author = models.CharField(max_length=250)
     isbn = models.CharField(max_length=250)
 
